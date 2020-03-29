de6dae280455036a3ca034688bf552141834dbe5
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-03-29 19:32:16 +0200

    Endpoint mostrar todos los generos creado

--- routes/generos.js
+++ routes/generos.js
@@ -2,5 +2,18 @@ const router = require('express').Router();
 const { Genero, Pelicula } = require('../models/index.js');
 
 
+//Mostrar todos los generos
+router.get('/', (req, res) => {
+    Genero.findAll({
+            include: [Pelicula]
+        })
+        .then(generos => res.send(generos))
+        .catch(err => {
+            console.log(err)
+            res.status(500).send({
+                message: 'Ha habido un problema al cargar los generos'
+            });
+        })
+})
 
 module.exports = router;
