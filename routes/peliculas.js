048b0561a5542af0bd3fea10c8026c9a795e0e08
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-03-29 19:34:51 +0200

    Endpoint mostrar todas las peliculas creado

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
-const { Pelicula } = require('../models/index.js');
+const { Pelicula, Genero } = require('../models/index.js');
 
 
+//Mostrar todas las peliculas
+router.get('/', (req, res) => {
+    Pelicula.findAll({
+            include: [Genero]
+        })
+        .then(peliculas => res.send(peliculas))
+        .catch(err => {
+            console.log(err)
+            res.status(500).send({
+                message: 'Ha habido un problema al cargar las peliculas'
+            });
+        })
+})
 
 
 module.exports = router;
